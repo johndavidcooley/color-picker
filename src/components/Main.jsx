b1cb46455956b08bff60fdf19beb61e1196e5aeb
John Cooley <johndavidcooley@gmail.com>
2020-07-11 01:11:00 -0400

    Initial commit.

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect} from 'react';
+
+const Main = () => {
+
+  const [twoFiftySixSquared, setTwoFiftySixSquared] = useState([]);
+
+  return (
+    <div className="main">
+
+    </div>
+  );
+};
+
+export default Main;
